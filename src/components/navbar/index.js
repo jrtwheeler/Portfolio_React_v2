cb03de309fd264d42b509452c6b0e70c7828cb9b
jrtwheeler <jwheele1@gmail.com>
2021-06-23 14:35:32 -0500

    Added header folder and file and updated component folder structure

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../mn_portrait.jpg";
+import logo from "../../mn_portrait.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {faBars} from "@fortawesome/free-solid-svg-icons";
 
