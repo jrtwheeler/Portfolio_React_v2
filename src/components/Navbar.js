b33e91b1b5a066b5082d82ac52f2189ca5e07b26
jrtwheeler <jwheele1@gmail.com>
2021-06-22 21:46:47 -0500

    Added image in a circular box

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,11 @@
 import React from "react";
-import logo from "../logo.svg";
+import logo from "../mn_portrait.jpg";
 
 const Navbar = () => {
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-                <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="logo..." /></a>
+                <a className="navbar-brand" href="#"><img className="avatar" src={logo} alt="logo..." /></a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
