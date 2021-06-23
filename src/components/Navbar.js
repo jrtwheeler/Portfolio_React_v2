b8e61b2ddadb0c12e9f28bfd852544e15b31058c
jrtwheeler <jwheele1@gmail.com>
2021-06-22 22:13:10 -0500

    The navbar has a photo and a focus and hover feature

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,7 @@
 import React from "react";
 import logo from "../mn_portrait.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faBars} from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
     return (
@@ -7,7 +9,7 @@ const Navbar = () => {
             <nav className="navbar navbar-expand-lg navbar-light bg-dark">
                 <a className="navbar-brand" href="#"><img className="avatar" src={logo} alt="logo..." /></a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
+                    <FontAwesomeIcon icon={faBars}/>
                 </button>
 
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
