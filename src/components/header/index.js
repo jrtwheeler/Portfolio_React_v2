6e03c083f82a184bd3b6408c4e46a52b7099dda7
jrtwheeler <jwheele1@gmail.com>
2021-06-23 15:35:01 -0500

    Added typed-text to header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "./style.css";
+import Typed from "react-typed";
 
 const Header = () => {
     return (
         <div className="header-wrapper">
             <div className="main-info">
                 <h1>web development and website promotions</h1>
+                <Typed
+                    className="typed-text"
+                    strings={["Web Design", "Web Development", "Facebook Ads SMM", "Google Ads"]}
+                    typeSpeed={40}
+                    backSpeed={60}
+                    loop
+                />
+                <a href="#" className="btn-main-offer"/>
             </div>
         </div>
     )
