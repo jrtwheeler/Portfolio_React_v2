51451fd9a43996aa35a31f4e1f78977f08095be1
jrtwheeler <jwheele1@gmail.com>
2021-06-22 21:36:39 -0500

    Revised navbar css styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,37 +4,35 @@ import logo from "../logo.svg";
 const Navbar = () => {
     return (
         <div>
-            <div className="container">
-                <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-                    <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="logo..." /></a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
+            <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+                <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="logo..." /></a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
 
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav ml-auto">
-                            <li className="nav-item active">
-                                <a className="nav-link" href="#">Home</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">About Me</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Services</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Experience</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Protfolio</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Contacts</a>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
-            </div>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">About Me</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Services</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Experience</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Portfolio</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Contacts</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         </div>
     )
 }
