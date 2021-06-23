c8b5988fdcd3cd695671147edfcc3911dbbfa97d
jrtwheeler <jwheele1@gmail.com>
2021-06-23 15:07:57 -0500

    Styled css for header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/navbar";
+import Header from "./components/header";
 
 function App() {
   return (
     <h2>
       <Navbar/>
+      <Header/>
     </h2>
   );
 }
---
+++ src/components/header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "style.css";
+const Header = () => {
+    return (
+        <div className="header-wrapper">
+            <div className="main-info">
+                <h1>web development and website promotions</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
