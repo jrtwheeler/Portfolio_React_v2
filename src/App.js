f609afdb78dc4d35de767960925ca0921e220d7f
jrtwheeler <jwheele1@gmail.com>
2021-06-24 12:15:13 -0500

    Added particles to the main page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Particles from 'react-particles-js';
 import Navbar from "./components/navbar";
 import Header from "./components/header";
 
 function App() {
   return (
     <h2>
-      <Navbar/>
-      <Header/>
+      <Particles
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 900
+              }
+            }
+          }
+        }}
+      />
+      <Navbar />
+      <Header />
     </h2>
   );
 }
