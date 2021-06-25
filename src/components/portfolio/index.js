d82b7f1c794a836b7853ee67983747c3d6140ea3
jrtwheeler <jwheele1@gmail.com>
2021-06-25 18:55:31 -0500

    Added portfolio photos and styled them

--- src/components/portfolio/index.js
+++ src/components/portfolio/index.js
@@ -1,4 +1,8 @@
 import React from "react";
+import "./style.css";
+import Walter from "../../assets/walters_app.PNG";
+import Bike from "../../assets/bike_shop.PNG";
+import Pour from "../../assets/pour_decision.jpg";
 
 const Portfolio = () => {
     return (
@@ -6,7 +10,18 @@ const Portfolio = () => {
             <div className="container">
                 <h1 className="text-uppercase text-center py-5">portfolio</h1>
                 <div className="image-box-wrapper row justify-content-center">
-                    <img className="portfolio-image" src="" alt=""/>
+                    <div className="portfolio-image-box">
+                        <img className="portfolio-image" src={Walter} alt="Walters Art Gallery Project" />
+                        <div className="overflow"></div>
+                    </div>
+                    <div className="portfolio-image-box">
+                        <img className="portfolio-image" src={Bike} alt="Bike Shop Project" />
+                        <div className="overflow"></div>
+                    </div>
+                    <div className="portfolio-image-box">
+                        <img className="portfolio-image" src={Pour} alt="Pour Decisions Project" />
+                        <div className="overflow"></div>
+                    </div>
                 </div>
             </div>
         </div>
