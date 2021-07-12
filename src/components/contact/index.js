fd1b6fb87f5a16d8d18db1e52eb317633d061a8e
jrtwheeler <jwheele1@gmail.com>
2021-07-12 00:03:27 -0400

    Form page is built

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -1,60 +1,125 @@
-import React from "react";
+import React, { useState } from "react";
+import emailjs from "emailjs-com";
+import { useForm } from "react-hook-form";
+import env from "react-dotenv";
 import "./style.css";
 
 const Contacts = () => {
-    return (
-        <div className="contacts">
-            <div className="text-center">
-                <h1>contact me</h1>
-                <p></p>
-            </div>
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-6 col-xs-12">
-                        {/* NAME */}
-                        <input
-                            id="name"
-                            type="text"
-                            className="form-control"
-                            placeholder="Name"
-                            name="name" />
-                        {/* PHONE */}
-                        <input
-                            id=""
-                            type="text"
-                            className="form-control"
-                            placeholder="Phone Number"
-                            name="phone" />
-                        {/* EMAIL */}
-                        <input
-                            id="email"
-                            type="email"
-                            className="form-control"
-                            placeholder="Email"
-                            name="email" />
-                        {/* SUBJECT */}
-                        <input
-                            id="subject"
-                            type="text"
-                            className="form-control"
-                            placeholder="Subject"
-                            name="subject" />
-                    </div>
-                    <div className="col-md-6 col-xs-12">
-                        <textarea
-                            id="description"
-                            type="text"
-                            className="form-control"
-                            placeholder="Email"
-                            email="email"
-                        ></textarea>
-                        <button className="btn-main-offer contact-btn" type="submit">contact me</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-        
+  const [successMessage, setSuccessMessage] = useState("");
+  const { register, handleSubmit, errors } = useForm();
+  const userID = env.userID;
+  const templateID = env.templateID;
+  const serviceID = env.serviceID;
+
+
+  const onSubmit = (data, r) => {
+    sendEmail(
+      serviceID,
+      templateID,
+      {
+        name: data.name,
+        phone: data.phone,
+        email: data.email,
+        subject: data.subject,
+        description: data.description
+      },
+      userID
     )
+    r.target.reset();
+  }
+
+  const sendEmail = (serviceID, templateID, variables, userID) => {
+    emailjs.send(serviceID, templateID, variables, userID)
+      .then(() => {
+        setSuccessMessage("Form sent successfully! I'll contact you as soon as possible.");
+      }).catch(err => console.error(`Something went wrong ${err}`));
+  }
+
+  return (
+    <div className="contacts">
+      <div className="text-center">
+        <h1>contact me</h1>
+        <p>Please fill out the form and describe you project needs and I'll contact you as soon as possible.</p>
+        <span className="success-message">{successMessage}</span>
+      </div>
+      <div className="container">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="row">
+            <div className="col-md-6 col-xs-12">
+              {/* NAME INPUT */}
+              <div className="text-center">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Name"
+                  name="name"
+                />
+                <div className="line"></div>
+              </div>
+              {/* <span className="error-message">
+                {errors.name && errors.name.message}
+              </span> */}
+              {/* PHONE INPUT */}
+              <div className="text-center">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Phone Number"
+                  name="phone"
+                />
+                <div className="line"></div>
+              </div>
+              {/* <span className="error-message">
+                {errors.phone && errors.phone.message}
+              </span> */}
+              {/* EMAIL INPUT */}
+              <div className="text-center">
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="Email"
+                  name="email"
+                />
+                <div className="line"></div>
+              </div>
+              {/* <span className="error-message">
+                {errors.email && errors.email.message}
+              </span> */}
+              {/* SUBJECT INPUT */}
+              <div className="text-center">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Subject"
+                  name="subject"
+                />
+                <div className="line"></div>
+              </div>
+              {/* <span className="error-message">
+                {errors.subject && errors.subject.message}
+              </span> */}
+            </div>
+            <div className="col-md-6 col-xs-12">
+              {/* DESCRIPTION */}
+              <div className="text-center">
+                <textarea
+                  type="text"
+                  className="form-control"
+                  placeholder="Please describe shortly you project..."
+                  name="description"
+                ></textarea>
+                <div className="line"></div>
+              </div>
+              {/* <span className="error-message">
+                {errors.description && errors.description.message}
+              </span> */}
+              <button className="btn-main-offer contact-btn" type="submit">contact me</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default Contacts;
